1026063b344b46e00a145c9a1cdebdb162b0da67
Sabrina Zancotti <zancotti@gmail.com>
2021-11-03 12:31:22 +0100

    removed import links

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,19 +3,27 @@ import "./searchBar.css";
 
 //frame done, need to add props accordingly (also need to create var colors in css)
 
-export const SearchBar = ({ primaryColor, secondaryColor }) => {
+export const SearchBar = ({
+  primaryColor = "black",
+  secondaryColor = "green",
+}) => {
   return (
     <form className="search-bar" action="/" method="get">
       <label htmlFor="search">
         <span className="search-bar-hidden">Search</span>
       </label>
       <input
+        backgroundColor={primaryColor}
         type="text"
         id="search"
         placeholder="&#xF002;&emsp; type something..." //have to add font awesome
         name="s"
       />
-      <button className="search-bar-button" type="submit">
+      <button
+        className="search-bar-button"
+        type="submit"
+        backgroundColor={secondaryColor}
+      >
         &nbsp;find
       </button>
     </form>
